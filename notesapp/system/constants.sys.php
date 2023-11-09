1d19314364fa34392b797a40b8dae153ca38f86c
Jude Okpara <counselme4@gmail.com>
2023-11-09 08:52:42 +0100

    [Updated] Changed and added new definitions

--- notesapp/system/constants.sys.php
+++ notesapp/system/constants.sys.php
@@ -3,6 +3,11 @@
  * constants.sys.php -- Defined Constants
  */
 
+// General Definitions
+define( 'SITE_NAME', 'NotesApp' ); // Site Name - Long
+define( 'SITE_NAME_SHORT', 'NA' ); // Site Name - Short/Initials
+
+
 // Directory Definitions
 define( 'APP_URL', $base_url .'/' );
 define( 'ASSETS_URL', $base_url .'/template_assets/' );
@@ -20,16 +25,15 @@ define( 'USER_LOGOUT', 'Auth.Logout' ); // Log Out
 define( 'USER_REGISTER', 'Auth.Register' ); // Register
 define( 'USER_RE_PASSWORD', 'Auth.ResetPassword' ); // Reset Password
 define( 'USER_UP_PASSWORD', 'Auth.UpdatePassword' ); // Update Password
-define( 'USER_HOME', 'Page.Dashboard' ); // Homepage/Dashboard
-define( 'USER_PROFILE', 'Account.Profile' ); // Account: Profile
-define( 'USER_BILLING', 'Account.Billing' ); // Account: Billing
-define( 'USER_SECURITY', 'Account.Security' ); // Account: Security
-define( 'USER_NOTIFICATIONS', 'Account.Notifications' ); // Account: Notifications
+define( 'USER_HOME', 'Page.Notes' ); // Homepage/Dashboard
+define( 'USER_NOTEBOOKS', 'Notes.Notebooks' ); // Notes: Notebooks
+define( 'USER_BIN', 'Notes.Bin' ); // Notes: Bin
+define( 'USER_PROFILE', 'Page.Profile' ); // Account: Profile
 
-
-// General Definitions
-define( 'SITE_NAME', 'IN-PROGRESS WEB APP' );
-define( 'SITE_NAME_SHORT', 'IP WEB APP' );
+//-- Status
+define( 'STA_404', 'Status.404' ); // Status: Error 404
+define( 'STA_500', 'Status.500' ); // Status: Error 404
+define( 'STA_DOWN', 'Status.Down' ); // Status: Down/Maintenance
 
 
 // API Keys
@@ -40,6 +44,7 @@ define( 'SITE_NAME_SHORT', 'IP WEB APP' );
 define( 'TBL_ADM', 'admin' ); // Admin table
 define( 'TBL_USR', 'users' ); // Users table
 define( 'TBL_ULOG', 'users_log' ); // Users Log table
+define( 'TBL_ULOGIND', 'users_login_data' ); // Users Login Data table
 define( 'TBL_UTOKEN', 'users_token' ); // Users Token table
 
 ?> 
