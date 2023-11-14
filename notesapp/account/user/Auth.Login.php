7fd9632ed895d0f9a83c013fafe743810bfd176f
Jude Okpara <counselme4@gmail.com>
2023-11-14 13:48:19 +0100

    [Updated] User Login Page

--- notesapp/account/user/Auth.Login.php
+++ notesapp/account/user/Auth.Login.php
@@ -45,6 +45,62 @@ if (isset($_SESSION['uid'])) { // currently logged in | uid: User ID
     <link rel="stylesheet" href="<?= ASSETS_URL; ?>assets/vendor/remixicon/fonts/remixicon.css">
     <link rel="stylesheet" href="<?= ASSETS_URL; ?>assets/vendor/@icon/dripicons/dripicons.css">
 
+    <!-- Extra CSS -->
+    <style>
+        /* with background */
+        /*
+        .input-container {
+            display: flex;
+            width: 100%;
+            margin-bottom: 15px;
+        }
+        .icon {
+            padding: 13px;
+            background: #f3f6fd;
+            color: #768492;
+            min-width: 50px;
+            text-align: center;
+            border-top: 1px solid #DCDDDF;
+            border-bottom: 1px solid #DCDDDF;
+            border-right: 1px solid #DCDDDF;
+            border-top-right-radius: 10px;
+            border-bottom-right-radius: 10px;
+        }
+        .input-field {
+            width: 100%;
+            padding: 10px;
+            outline: none;
+            border-right: 0!important;
+            border-top-right-radius: 0!important;
+            border-bottom-right-radius: 0!important;
+        }
+        */
+        /* without background */
+        .input-container {
+            display: flex;
+            width: 100%;
+            margin-bottom: 15px;
+        }
+        .input-container i {
+            position: relative;
+        }
+        .icon {
+            padding: 15px;
+            color: rgb(49, 0, 128);
+            min-width: 50px;
+            text-align: center;
+        }
+        .input-field {
+            width: 100%;
+            padding: 10px;
+        }
+        #showpass:hover {
+            cursor: pointer;
+            background-color: #f3f6fd;
+            border-radius: 100%;
+        }
+    </style>
+
 </head>
 <body class=" ">
 
@@ -60,7 +116,7 @@ if (isset($_SESSION['uid'])) { // currently logged in | uid: User ID
             <div class="container h-100">
                 <div class="row justify-content-center align-items-center height-self-center">
                     <div class="col-md-5 col-sm-12 col-12 align-self-center">
-                        <div class="sign-user_card">
+                        <div class="sign-user_card" id="auth">
                             <div class="logo-detail">
                                 <div class="d-flex align-items-center"><img src="<?= ASSETS_URL; ?>assets/images/logo.png" class="img-fluid rounded-normal light-logo logo" alt="logo">
                                     <h4 class="logo-title ml-3"><?= $pageTitle; ?></h4>
@@ -70,9 +126,9 @@ if (isset($_SESSION['uid'])) { // currently logged in | uid: User ID
 							// redirect to a page
 							if ( isset($_GET['redirect_to']) AND !empty($_GET['redirect_to']) ){
 								$redirect_to_page = $_GET['redirect_to'];
-								echo "<input type='text' id='redirect_to' value='{$redirect_to_page}'>";
+								echo "<input type='hidden' id='redirect_to' value='{$redirect_to_page}'>";
 							} else {
-								echo "<input type='text' id='redirect_to' value='".USER_HOME."'>";
+								echo "<input type='hidden' id='redirect_to' value='".USER_HOME."'>";
 							}
 							?>
                             <h3 class="mb-2">Sign In</h3>
@@ -82,22 +138,20 @@ if (isset($_SESSION['uid'])) { // currently logged in | uid: User ID
                                 <div class="row">
                                     <div class="col-lg-12">
                                         <div class="floating-label form-group">
-                                            <input class="floating-input form-control" type="text" id="auth_name" name="auth_name" v-model="loginData.auth_name" placeholder="">
-                                            <label>Username</label>
+                                            <input class="floating-input form-control" type="text" id="username" name="username" v-model="loginData.username" placeholder="">
+                                            <label for="username">Username</label>
                                         </div>
                                     </div>
                                     <div class="col-lg-12">
-                                        <div class="floating-label form-group">
-                                            <input class="floating-input form-control" type="password" id="password" name="password" v-model="loginData.password" placeholder="">
-                                            <span class="input-group-text">
-                                                <input class="form-check-input" @click="showPass()" type="checkbox">
-                                            </span>
-                                            <label>Password</label>
+                                        <div class="floating-label form-group input-container">
+                                            <input class="floating-input form-control input-field" type="password" id="password" name="password" v-model="loginData.password" placeholder="">
+                                            <label for="password">Password</label>
+                                            <i class="fa fa-eye icon" id="showpass" onclick="showPass()"></i>
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="custom-control custom-checkbox mb-3 text-left">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck1" name="customCheck1">
                                             <label class="custom-control-label" for="customCheck1">Remember Me</label>
                                         </div>
                                     </div>
@@ -124,7 +178,7 @@ if (isset($_SESSION['uid'])) { // currently logged in | uid: User ID
 
 	<script>
         $(document).ready(function(){
-            $('#auth_name').focus();
+            $('#username').focus();
         });
     </script>
 
